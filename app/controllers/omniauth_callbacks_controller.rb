df27ba431a25dbbe54892e937d17ce1fc2c067f7
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-08 14:44:37 -0700

    adds omniauth callback controller action

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,3 +1,23 @@
-class OmniauthCallbacksController < ApplicationController 
-	
+class CallbacksController < Devise::OmniauthCallbacksController
+
+	def facebook
+  		@user = User.from_omniauth(request.env["omniauth.auth"])
+	  if @user.persisted?
+	    sign_in_and_redirect @user, :event => :authentication
+	  else
+	    session["devise.facebook_data"] = request.env["omniauth.auth"]
+	    redirect_to new_user_registration_url
+	  end
+	end
+
+    def google_oauth2
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+           if @user.persisted?
+              sign_in_and_redirect @user, :event => :authentication
+             else
+              session["devise.google_data"] = request.env["omniauth.auth"]
+              redirect_to new_user_registration_url
+             end
+    end
+
 end
